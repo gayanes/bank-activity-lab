f277e88b474bb3eb2ea3806a012711a962f55eb1
gayanes <gayanesance@gmail.com>
2023-11-09 02:22:55 -0500

    Added documentation to the class methods

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -14,7 +14,14 @@ class StockMetrics(StockData):
         self.load()
 
     def average01(self):
-        """pt1
+        """
+        Calculates the average of stock prices and returns a list.
+
+        The function converts strings to floats and ignores empty and white 
+        space strings, to create a list of valid numerics. It calculates 
+        the sum for the row, divides by the length of the list of numerics, 
+        and rounds the result to the third decimal place. It appends this 
+        value to the averages list.
         """
         averages = []
         for row in self.data:
@@ -29,7 +36,14 @@ class StockMetrics(StockData):
         return averages
 
     def median02(self):
-        """pt2
+        """
+        Calculates the median of stock prices and returns a list.
+        
+        The function converts strings to floats and ignores empty and white 
+        space strings, to create a list of valid numerics. It utilizes the 
+        median function in the statistics module to calculate the median for 
+        the row. It does not round the result and appends this value to the 
+        median list.
         """
         medians = []
         for row in self.data:
@@ -41,7 +55,14 @@ class StockMetrics(StockData):
         return medians
 
     def stddev03(self):
-        """pt3
+        """
+        Calculates the standard deviation of stock prices and returns a list.
+        
+        The function converts strings to floats and ignores empty and white 
+        space strings, to create a list of valid numerics. It utilizes the 
+        stdev function in the statistics module to calculate the standard 
+        deviation for the row. It does not round the result and appends this 
+        value to the std_devs list.
         """
         std_devs = []
         for row in self.data:
